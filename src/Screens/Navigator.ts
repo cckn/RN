e536fa36ef21f443c3f28da53369860dbfd0952f
DS <cckn1985@gmail.com>
2020-01-17 09:38:08 +0900

    네이게이터 작업 및 파일 생성

--- src/Screens/Navigator.ts
+++ src/Screens/Navigator.ts
@@ -1,6 +1,22 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation'
-import {
-  createBottomTabNavigator,
-  createMaterialTopTabNavigator,
-} from 'react-navigation-tabs'
 import { createStackNavigator } from 'react-navigation-stack'
+
+import CheckLogin from '~/Screens/CheckLogin'
+import Login from '~/Screens/Login'
+import MovieHome from '~/Screens/MovieHome'
+import MovieDetail from '~/Screens/MovieDetail'
+
+const LoginNavigator = createStackNavigator({ Login })
+
+const MovieNavigator = createStackNavigator({ MovieHome, MovieDetail })
+
+const AppNavigator = createSwitchNavigator(
+  {
+    CheckLogin,
+    LoginNavigator,
+    MovieNavigator,
+  },
+  { initialRouteName: 'CheckLogin' }
+)
+
+export default createAppContainer(AppNavigator)
